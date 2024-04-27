073b64cae3d18542b85c5374446f261318317462
Anthony LiFonti <alifonti21@gmail.com>
2024-04-27 09:39:38 -0500

    RoundsView edit mode

--- UpNorthMolkky/Views/Home/HistoryTab/RoundsTab/RoundsView.swift
+++ UpNorthMolkky/Views/Home/HistoryTab/RoundsTab/RoundsView.swift
@@ -10,15 +10,27 @@ import SwiftUI
 struct RoundsView: View {
     @Binding var rounds: [MolkkyRound]
     
+    @State var editMode = EditMode.inactive
+    
     var body: some View {
         VStack {
-            EditButton()
-            List($rounds, id: \.id, editActions: .delete) { $round in
-                NavigationLink(destination: ScoreboardView(round: $round)) {
-                    CardView(round: round)
+            HStack {
+                Spacer()
+                Button(editMode == EditMode.inactive ? "Edit" : "Done") {
+                    editMode = editMode == EditMode.inactive ? EditMode.active : EditMode.inactive
                 }
             }
+            .padding()
+            List {
+                ForEach($rounds, id: \.id) { $round in
+                    NavigationLink(destination: ScoreboardView(round: $round)) {
+                        CardView(round: round)
+                    }
+                }
+                .onDelete { rounds.remove(atOffsets: $0) }
+            }
             .listStyle(.automatic)
+            .environment(\.editMode, $editMode)
         }
     }
 }
