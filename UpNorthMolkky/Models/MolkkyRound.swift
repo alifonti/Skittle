d6f7cdee065bf1f0c0d2713e0b8bc25a5caec784
Anthony LiFonti <alifonti21@gmail.com>
2024-05-15 20:22:22 -0500

    Implement survivor award

--- UpNorthMolkky/Models/MolkkyRound.swift
+++ UpNorthMolkky/Models/MolkkyRound.swift
@@ -263,6 +263,16 @@ struct MolkkyRound: Identifiable, Codable, Hashable {
         // Reckless
         // Efficient
         // Survivor
+        var survivorData: [Contender] = []
+        round.contenderScores.forEach({
+            let range = $0.attempts.map({$0.score}).firstRange(of: Array(repeating: 0, count: round.missesForElimination - 1))
+            if (range != nil && range?.endIndex != $0.attempts.count && !$0.isEliminated) {
+                survivorData.append($0.contender)
+            }
+        })
+        if (survivorData.count > 0) {
+            awards.append((Award.survivor, survivorData, nil))
+        }
         // So close!
         let soCloseData = round.contenderScores.filter({$0.totalScore == round.targetScore - 1}).map({$0.contender})
         if (soCloseData.count > 0) {
