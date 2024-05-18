36729e6b1b3d95c99042e0f054c149e01be050a3
Anthony LiFonti <alifonti21@gmail.com>
2024-05-17 21:11:16 -0500

    Sort and delete rounds list

--- UpNorthMolkky/Views/Home/HistoryTab/RoundsTab/RoundsView.swift
+++ UpNorthMolkky/Views/Home/HistoryTab/RoundsTab/RoundsView.swift
@@ -12,6 +12,50 @@ struct RoundsView: View {
     @EnvironmentObject var navigationState: NavigationState
     @Binding var rounds: [MolkkyRound]
     
+    let sevenDaysAgo: Date = Calendar.current.date(byAdding: DateComponents(day: -7), to: .now) ?? .now
+    
+    var roundsToday: [Binding<MolkkyRound>] {
+        $rounds.filter({ $round in
+            Calendar.current.compare(round.date, to: .now, toGranularity: .day) == .orderedDescending ||
+            Calendar.current.compare(round.date, to: .now, toGranularity: .day) == .orderedSame
+        }).sorted(by: {a, b in
+            a.wrappedValue.date > b.wrappedValue.date
+        })
+    }
+    var roundsThisWeek: [Binding<MolkkyRound>] {
+        $rounds.filter({ $round in
+            Calendar.current.compare(round.date, to: .now, toGranularity: .day) == .orderedAscending &&
+            Calendar.current.compare(round.date, to: sevenDaysAgo, toGranularity: .day) == .orderedDescending
+        }).sorted(by: {a, b in
+            a.wrappedValue.date > b.wrappedValue.date
+        })
+    }
+    var roundsOlder: [Binding<MolkkyRound>] {
+        $rounds.filter({ $round in
+            Calendar.current.compare(round.date, to: sevenDaysAgo, toGranularity: .day) == .orderedSame ||
+            Calendar.current.compare(round.date, to: sevenDaysAgo, toGranularity: .day) == .orderedAscending
+        }).sorted(by: {a, b in
+            a.wrappedValue.date > b.wrappedValue.date
+        })
+    }
+    
+//    init(rounds: Binding<[MolkkyRound]>) {
+//        self._rounds = rounds
+//    }
+    
+//    func srtDates(a: Binding<MolkkyRound>, b: Binding<MolkkyRound>) {
+//        .sorted(by: {a, b in
+//            a.wrappedValue.date > b.wrappedValue.date
+//        })
+//    }
+    func deleteRound(offsets: IndexSet, list: [Binding<MolkkyRound>]) {
+        for offset in offsets {
+            if let index = rounds.firstIndex(where: {$0 == list[offset].wrappedValue}) {
+                rounds.remove(at: index)
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
             HStack(spacing: 20) {
@@ -21,17 +65,51 @@ struct RoundsView: View {
                 EditButton()
             }
             List {
-                ForEach($rounds, id: \.id) { $round in
-                    Button(action: {
-                        navigationState.activeRoundId = round.id
-                        navigationState.isNavigationActive = true
-                    }) {
-                        CardView(round: round)
-                            .deleteDisabled(!(editMode?.wrappedValue.isEditing ?? false))
+                if (roundsToday.count > 0) {
+                    Section(header: Text("Today")) {
+                        ForEach(roundsToday, id: \.id) { $round in
+                            Button(action: {
+                                navigationState.activeRoundId = round.id
+                                navigationState.isNavigationActive = true
+                            }) {
+                                CardView(round: round)
+                                    .deleteDisabled(!(editMode?.wrappedValue.isEditing ?? false))
+                            }
+                        }
+                        .onDelete(perform: {deleteRound(offsets: $0, list: roundsToday)})
+                        .listRowBackground(Color(named: "s.fill.tertiary"))
                     }
                 }
-                .onDelete { rounds.remove(atOffsets: $0) }
-                .listRowBackground(Color(named: "s.fill.tertiary"))
+                if (roundsThisWeek.count > 0) {
+                    Section(header: Text("This week")) {
+                        ForEach(roundsThisWeek, id: \.id) { $round in
+                            Button(action: {
+                                navigationState.activeRoundId = round.id
+                                navigationState.isNavigationActive = true
+                            }) {
+                                CardView(round: round)
+                                    .deleteDisabled(!(editMode?.wrappedValue.isEditing ?? false))
+                            }
+                        }
+                        .onDelete(perform: {deleteRound(offsets: $0, list: roundsThisWeek)})
+                        .listRowBackground(Color(named: "s.fill.tertiary"))
+                    }
+                }
+                if (roundsOlder.count > 0) {
+                    Section(header: Text("Older")) {
+                        ForEach(roundsOlder, id: \.id) { $round in
+                            Button(action: {
+                                navigationState.activeRoundId = round.id
+                                navigationState.isNavigationActive = true
+                            }) {
+                                CardView(round: round)
+                                    .deleteDisabled(!(editMode?.wrappedValue.isEditing ?? false))
+                            }
+                        }
+                        .onDelete(perform: {deleteRound(offsets: $0, list: roundsOlder)})
+                        .listRowBackground(Color(named: "s.fill.tertiary"))
+                    }
+                }
             }
             .scrollContentBackground(.hidden)
             .background(Color(named: "s.background.primary"))
